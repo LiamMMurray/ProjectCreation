174fa325de609f2a79762b0d12190ab885a1d205
Liam <jp@h.com>
2019-05-07 14:12:29 -0400

    adds project with basic main source file

---
+++ ProjectCreation/ProjectCreation/Main.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+int main()
+{
+	// placeholder io for test purposes until rendering/window io implemented
+	std::string response;
+	std::cout << "ProjectCreation";
+	std::cin >> response;
+	
+	return 0;
+}
