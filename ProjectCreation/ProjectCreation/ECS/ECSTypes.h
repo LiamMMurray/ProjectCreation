c267af9d7a6f17e3082465a2d060278533c7a456
Liam Murray <lmmurray@student.fullsail.edu>
2019-05-08 05:34:52 -0400

    adds error type

--- ProjectCreation/ProjectCreation/ECS/ECSTypes.h
+++ ProjectCreation/ProjectCreation/ECS/ECSTypes.h
@@ -11,3 +11,19 @@ struct TypeId
                 return temp;
         }
 };
+enum ERESULT_FLAG
+{
+        SUCCESS      = 0b00000000,
+        PLACEHOLDER1 = 0b00000001,
+        PLACEHOLDER2 = 0b00000010,
+        PLACEHOLDER3 = 0b00000100,
+        PLACEHOLDER4 = 0b00001000,
+        PLACEHOLDER5 = 0b00010000,
+        PLACEHOLDER6 = 0b00100000,
+        PLACEHOLDER7 = 0b01000000,
+        PLACEHOLDER8 = 0b10000000
+};
+struct EResult
+{
+        uint64_t m_Flags;
+};
