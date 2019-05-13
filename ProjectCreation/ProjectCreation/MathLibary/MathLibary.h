cee73473f9ea9cd842b0eab154951bdb37030526
WanCheeKiang <wwkiang@student.fullsail.edu>
2019-05-13 12:39:33 -0400

    Branch Update
    stage file to update the branch

--- ProjectCreation/ProjectCreation/MathLibary/MathLibary.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include "Transform.h"
-class MathLibary
-{
-
-};
--- ProjectCreation/ProjectCreation/MathLibary/Vector.cpp
+++
@@ -1,8 +0,0 @@
-#include "Vector.h"
-
-using namespace DirectX;
-
-const XMVECTOR VectorConstants::Forward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
-const XMVECTOR VectorConstants::Right   = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
-const XMVECTOR VectorConstants::Up      = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
-const XMVECTOR VectorConstants::Zero    = XMVectorSet(.0f, 0.0f, 0.0f, 0.0f);
