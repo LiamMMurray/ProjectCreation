c08c1e8dcd26cc044c75a58faef130f14f50ae8d
WanCheeKiang <wwkiang@student.fullsail.edu>
2019-05-13 12:36:36 -0400

    math Library function update
    LookAt, TurnTo, tranfromlerp, orthoNormalize, and common math function

--- ProjectCreation/ProjectCreation/Rendering/Vertex.h
+++ ProjectCreation/ProjectCreation/Rendering/Vertex.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <DirectXMath.h>
+#include"../MathLibrary/MathConstants.h"
 struct FVertex
 {
         DirectX::XMFLOAT3 position;
@@ -7,7 +8,7 @@ struct FVertex
         DirectX::XMFLOAT2 uv;
         DirectX::XMFLOAT3 normal;
         DirectX::XMFLOAT3 tangent;
-        DirectX::XMFLOAT3 binnormal;
+        DirectX::XMFLOAT3 binormal;
 
         FVertex() = default;
         FVertex(DirectX::XMFLOAT3 position, DirectX::XMFLOAT4 color);
@@ -30,5 +31,45 @@ struct FVertex
                 return out;
         };
 };
-struct SkinnedVertex
-{};
+struct FSkinnedVertex
+{
+        DirectX::XMFLOAT3 position;
+        DirectX::XMFLOAT4 color;
+        DirectX::XMFLOAT2 uv;
+        DirectX::XMFLOAT3 normal;
+        DirectX::XMFLOAT3 tangent;
+        DirectX::XMFLOAT3 binormal;
+
+        DirectX::XMUINT4  jointIndices;
+        DirectX::XMFLOAT4 weights;
+
+
+        FSkinnedVertex() = default;
+
+        FSkinnedVertex(DirectX::XMFLOAT3 _p, DirectX::XMFLOAT4 _c);
+
+        inline bool operator==(const FSkinnedVertex& rhs) const
+        {
+                const FSkinnedVertex& lhs = *this;
+
+                bool out = (fabsf(lhs.position.x - rhs.position.x) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.position.y - rhs.position.y) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.position.z - rhs.position.z) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.normal.x - rhs.normal.x) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.normal.y - rhs.normal.y) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.normal.z - rhs.normal.z) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.uv.x - rhs.uv.x) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.uv.y - rhs.uv.y) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.color.x - rhs.color.x) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.color.y - rhs.color.y) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.color.z - rhs.color.z) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.color.w - rhs.color.w) < MathConstants::kEpsilon) &&
+                           (lhs.jointIndices.x == rhs.jointIndices.x) && (lhs.jointIndices.y == rhs.jointIndices.y) &&
+                           (lhs.jointIndices.z == rhs.jointIndices.z) && (lhs.jointIndices.w == rhs.jointIndices.w) &&
+                           (fabsf(lhs.weights.x - rhs.weights.x) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.weights.y - rhs.weights.y) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.weights.z - rhs.weights.z) < MathConstants::kEpsilon) &&
+                           (fabsf(lhs.weights.w - rhs.weights.w) < MathConstants::kEpsilon);
+                return out;
+        };
+};
