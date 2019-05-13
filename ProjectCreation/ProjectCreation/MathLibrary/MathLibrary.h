a2fa15c20b026e1592ff379e1342dad07f990ed1
WanCheeKiang <wwkiang@student.fullsail.edu>
2019-05-13 13:31:41 -0400

    Update orthonormalize
    static function update

--- ProjectCreation/ProjectCreation/MathLibrary/MathLibrary.h
+++ ProjectCreation/ProjectCreation/MathLibrary/MathLibrary.h
@@ -15,7 +15,7 @@ class MathLibrary
                 return val - (max - min) * floor(val / (max - min));
         }
         // Matrix Function
-        void              OrthoNormalize(DirectX::XMVECTOR normal, DirectX::XMVECTOR& tangent);
+        static void              OrthoNormalize(DirectX::XMVECTOR normal, DirectX::XMVECTOR& tangent);
         DirectX::XMMATRIX LookAt(DirectX::XMVECTOR vPos, DirectX::XMVECTOR tPos, DirectX::XMVECTOR up);
         void              TurnTo(DirectX::XMMATRIX& matrix, DirectX::XMVECTOR targetPosition, float speed);
 };
