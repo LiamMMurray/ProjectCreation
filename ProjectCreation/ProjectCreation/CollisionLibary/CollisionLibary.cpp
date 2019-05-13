0221f232309749887246b376fb3aff4e27abee36
WanCheeKiang <wwkiang@student.fullsail.edu>
2019-05-13 16:44:40 -0400

    add collision function

--- ProjectCreation/ProjectCreation/CollisionLibary/CollisionLibary.cpp
+++ ProjectCreation/ProjectCreation/CollisionLibary/CollisionLibary.cpp
@@ -1,5 +1,30 @@
 #include "CollisionLibary.h"
+#include "../MathLibrary/MathLibrary.h"
 using namespace DirectX;
+
+CollisionComponent::FPlane CollisionLibary::CalculatePlane(DirectX::XMVECTOR a, DirectX::XMVECTOR b, DirectX::XMVECTOR c)
+{
+        CollisionComponent::FPlane output;
+        output.normal = XMVector3Normalize(XMVector3Cross((b - a), (c - a)));
+        output.offset = XMVectorGetX(XMVector3Dot(output.normal, c));
+
+        return output;
+}
+
+CollisionComponent::FCotactPoint CollisionLibary::CollidePoint(DirectX::XMVECTOR pointA,
+                                                               DirectX::XMVECTOR pointB,
+                                                               float             radius) // raduis must bne pointB's radius
+{
+
+        CollisionComponent::FCotactPoint output;
+
+        XMVECTOR tempDir    = XMVector3Normalize(pointA - pointB);
+        XMVECTOR contactPos = pointB + tempDir * radius;
+        output.position     = contactPos;
+        output.normal       = tempDir;
+
+        return output;
+}
 
 CollisionComponent::FCollideResult CollisionLibary::OverlapSphereToSphere(CollisionComponent::FSphere& a,
                                                                           CollisionComponent::FSphere& b)
@@ -18,18 +43,18 @@ CollisionComponent::FCollideResult CollisionLibary::OverlapSphereToSphere(Collis
         }
         else if (distance == totalRadius)
         {
-                output.collisionType  = CollisionComponent::ECollisionType::ECollide;
-                XMVECTOR tempDir      = XMVector3Normalize(a.center - b.center);
+                output.collisionType = CollisionComponent::ECollisionType::ECollide;
+                /*XMVECTOR tempDir      = XMVector3Normalize(a.center - b.center);
                 XMVECTOR contactPos   = b.center + tempDir * b.radius;
                 contactPoint.position = contactPos;
-                contactPoint.normal   = tempDir;
-                output.collideSurfaces.push_back(contactPoint);
+                contactPoint.normal   = tempDir;*/
+                output.collideSurfaces.push_back(CollidePoint(a.center, b.center, b.radius));
 
-                XMVECTOR tempDirA      = XMVector3Normalize(b.center - a.center);
-                XMVECTOR contactPosA   = a.center + tempDir * a.radius;
-                contactPoint.position = contactPosA;
-                contactPoint.normal   = tempDirA;
-                output.collideSurfaces.push_back(contactPoint);
+                /*    XMVECTOR tempDirA     = XMVector3Normalize(b.center - a.center);
+                    XMVECTOR contactPosA  = a.center + tempDir * a.radius;
+                    contactPoint.position = contactPosA;
+                    contactPoint.normal   = tempDirA;*/
+                output.collideSurfaces.push_back(CollidePoint(b.center, a.center, a.radius));
         }
 
 
@@ -41,13 +66,78 @@ CollisionComponent::FCollideResult CollisionLibary::SweepSphereToSphere(Collisio
                                                                         CollisionComponent::FSphere& checkB)
 {
         CollisionComponent::FCollideResult output;
+        CollisionComponent::FCotactPoint   contactPoint;
         CollisionComponent::FCapsule       capsule;
-        XMVECTOR                           cloestPoint;
+
         capsule.startPoint = startA.center;
         capsule.endPoint   = endA.center;
-        capsule.radius = startA.radius;
+        capsule.radius     = startA.radius;
 
-		float totalRadius = capsule.radius + checkB.radius;
-		
+        XMVECTOR cloestPoint = MathLibrary::GetClosestPointFromLine(capsule.startPoint, capsule.endPoint, checkB.center);
+        float    totalRadius = capsule.radius + checkB.radius;
+        float    distance    = XMVectorGetX(XMVector3Dot(cloestPoint, checkB.center));
+
+        if (distance == totalRadius)
+        {
+                output.collisionType = CollisionComponent::ECollisionType::ECollide;
+                output.collideSurfaces.push_back(CollidePoint(checkB.center, cloestPoint, capsule.radius));
+
+                output.collideSurfaces.push_back(CollidePoint(cloestPoint, checkB.center, checkB.radius));
+        }
+        else if (distance < totalRadius)
+        {
+                output.collisionType = CollisionComponent::ECollisionType::EOveralap;
+        }
+        else if (distance > totalRadius)
+        {
+                output.collisionType = CollisionComponent::ECollisionType::ENoCollision;
+        }
+        return output;
+}
+
+CollisionComponent::FCollideResult CollisionLibary::SphereToAabb(CollisionComponent::FSphere& sphere,
+                                                                 CollisionComponent::FAabb&   aabb)
+{
+        CollisionComponent::FCollideResult output;
+        XMVECTOR                           aabbMin           = aabb.center - aabb.extents;
+        XMVECTOR                           aabbMax           = aabb.center + aabb.extents;
+        XMVECTOR                           cloestPointinAABB = XMVectorMin(XMVectorMax(sphere.center, aabbMin), aabbMax);
+        float                              distance;
+        return output;
+}
+
+CollisionComponent::FCollideResult CollisionLibary::AabbToAabb(CollisionComponent::FAabb& a, CollisionComponent::FAabb& b)
+{
+        CollisionComponent::FCollideResult output;
+
+		XMVECTOR aMax = a.center + a.extents;
+        XMVECTOR aMin = a.center - a.extents;
+        XMVECTOR bMax = b.center + b.extents;
+        XMVECTOR bMin = b.center - b.extents;
+
+        float aMaxX = XMVectorGetX(aMax);
+        float aMaxY = XMVectorGetY(aMax);
+        float aMaxZ = XMVectorGetZ(aMax);
+
+        float aMinX = XMVectorGetX(aMin);
+        float aMinY = XMVectorGetY(aMin);
+        float aMinZ = XMVectorGetZ(aMin);
+
+        float bMaxX = XMVectorGetX(bMax);
+        float bMaxY = XMVectorGetY(bMax);
+        float bMaxZ = XMVectorGetZ(bMax);
+
+        float bMinX = XMVectorGetX(bMin);
+        float bMinY = XMVectorGetY(bMin);
+        float bMinZ = XMVectorGetZ(bMin);
+
+        if (aMaxX < bMinX || aMinX > bMaxX)
+               output.collisionType = CollisionComponent::ENoCollision;
+        if (aMaxY < bMinY || aMinY > bMaxY)
+                output.collisionType = CollisionComponent::ENoCollision;
+        if (aMaxZ < bMinZ || aMinZ > bMaxZ)
+                output.collisionType = CollisionComponent::ENoCollision;
+
+		 output.collisionType = CollisionComponent::EOveralap;
         return output;
 }
--- ProjectCreation/ProjectCreation/CollisionLibary/CollisionLibary.h
+++ ProjectCreation/ProjectCreation/CollisionLibary/CollisionLibary.h
@@ -3,11 +3,12 @@
 class CollisionLibary
 {
         CollisionComponent::FPlane CalculatePlane(DirectX::XMVECTOR a, DirectX::XMVECTOR b, DirectX::XMVECTOR c);
+        CollisionComponent::FCotactPoint   CollidePoint(DirectX::XMVECTOR pointA, DirectX::XMVECTOR pointB, float radius);
     public:
-        void CalculateFrustum(CollisionComponent::Frustum& frustum);
         CollisionComponent::FCollideResult OverlapSphereToSphere(CollisionComponent::FSphere& a, CollisionComponent::FSphere& b); 
         CollisionComponent::FCollideResult SweepSphereToSphere(CollisionComponent::FSphere& startA,
                                                                CollisionComponent::FSphere& endA, CollisionComponent::FSphere& checkB); 
 		CollisionComponent::FCollideResult SphereToAabb(CollisionComponent::FSphere& sphere, CollisionComponent::FAabb& aabb);
+        CollisionComponent::FCollideResult AabbToAabb(CollisionComponent::FAabb& a, CollisionComponent::FAabb& b);
        
 };
