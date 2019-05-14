4c7791ac322dd8cd08e1dbf3d22c79bee3108e0d
Unknown <wwkiang@student.fullsail.edu>
2019-05-14 17:49:46 -0400

    Collision function testing
    testing the sweep sphere to sphere function: works

--- ProjectCreation/ProjectCreation/CollisionLibary/CollisionLibary.h
+++ ProjectCreation/ProjectCreation/CollisionLibary/CollisionLibary.h
@@ -1,14 +1,17 @@
 #pragma once
-#include"CollisionComponent.h"
+#include "CollisionComponent.h"
 class CollisionLibary
 {
-        CollisionComponent::FPlane CalculatePlane(DirectX::XMVECTOR a, DirectX::XMVECTOR b, DirectX::XMVECTOR c);
-        CollisionComponent::FCotactPoint   CollidePoint(DirectX::XMVECTOR pointA, DirectX::XMVECTOR pointB, float radius);
+        CollisionComponent::FPlane              CalculatePlane(DirectX::XMVECTOR a, DirectX::XMVECTOR b, DirectX::XMVECTOR c);
+        static CollisionComponent::FCotactPoint CollidePoint(DirectX::XMVECTOR pointA, DirectX::XMVECTOR pointB, float radius);
+
     public:
-        CollisionComponent::FCollideResult OverlapSphereToSphere(CollisionComponent::FSphere& a, CollisionComponent::FSphere& b); 
-        CollisionComponent::FCollideResult SweepSphereToSphere(CollisionComponent::FSphere& startA,
-                                                               CollisionComponent::FSphere& endA, CollisionComponent::FSphere& checkB); 
-		CollisionComponent::FCollideResult SphereToAabb(CollisionComponent::FSphere& sphere, CollisionComponent::FAabb& aabb);
-        CollisionComponent::FCollideResult AabbToAabb(CollisionComponent::FAabb& a, CollisionComponent::FAabb& b);
-       
+        static CollisionComponent::FCollideResult OverlapSphereToSphere(CollisionComponent::FSphere& a,
+                                                                        CollisionComponent::FSphere& b);
+        static CollisionComponent::FCollideResult SweepSphereToSphere(CollisionComponent::FSphere& startA,
+                                                                      CollisionComponent::FSphere& endA,
+                                                                      CollisionComponent::FSphere& checkB);
+        static CollisionComponent::FCollideResult SphereToAabb(CollisionComponent::FSphere& sphere,
+                                                               CollisionComponent::FAabb&   aabb);
+        static CollisionComponent::FCollideResult AabbToAabb(CollisionComponent::FAabb& a, CollisionComponent::FAabb& b);
 };
